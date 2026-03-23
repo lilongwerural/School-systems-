91fdc56d5c8a74e06660fc40614f00f3c8f8bfbb
lilongwerural <lilongwerural@gmail.com>
2026-03-23 10:54:12 +0200

    Delete env-config.js

--- env-config.js
+++
@@ -1,5 +0,0 @@
-// This file is auto-generated during GitHub Actions deployment.
-// Do NOT edit manually. Do NOT commit real credentials here.
-// Credentials are injected from GitHub Secrets at build time.
-window.ENV_SUPABASE_URL = '';
-window.ENV_SUPABASE_ANON_KEY = '';
